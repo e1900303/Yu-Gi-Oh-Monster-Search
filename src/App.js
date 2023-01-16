d365ca39b64755b7dd260a420495430a2509c239
unknown <tranduyphat080300@gmail.com>
2023-01-16 21:00:03 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+// import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function getRandomValues(arr, count) {
+//   var shuffled = arr.slice(0), i = arr.length, min = i - count, temp, index;
+//   while (i-- > min) {
+//     index = Math.floor((i + 1) * Math.random());
+//     temp = shuffled[index];
+//     shuffled[index] = shuffled[i];
+//     shuffled[i] = temp;
+//   }
+//   return shuffled.slice(min);
+// }
+
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: '',
+    };
+    console.log('constructor')
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount')
+    fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php?type=normal%20monster')
+      .then((response) => response.json())
+      .then((users) => this.setState(
+        () => {
+          // const randomIndex = Math.floor(Math.random() * (users.data.length - 10));
+          return { monsters: users.data }
+          // return { monsters: getRandomValues(users.data, 10) }
+        },
+        () => {
+          console.log(this.state)
+        }
+      ))
+    //   axios.get('https://jsonplaceholder.typicode.com/users')
+    //     .then((users) => this.setState({ monsters: users.data }))
+    //     .then(() => {
+    //       console.log(this.state)
+    //     })
+  }
+
+
+  render() {
+    console.log('render')
+    const filtredMonters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField)
+    })
+    return (
+      <div className='App' >
+        <input className='searchBox' type='search' placeholder='search monster' onChange={(event) => {
+          const searchField = event.target.value.toLocaleLowerCase();
+          this.setState(() => {
+            return { searchField }
+          })
+        }} />
+        {filtredMonters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          )
+        })}
+      </div>
+    );
+  };
 }
 
 export default App;
