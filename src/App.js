1747b5ce8637dadbc40a1f657e5c840054d965d7
unknown <tranduyphat080300@gmail.com>
2023-01-17 02:44:40 +0200

    convert to functional components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { Component } from 'react';
+import { React, useState, useEffect } from 'react';
 import CardList from './components/card-list.component';
 import SearchBox from './components/search-box.component';
 // import axios from 'axios';
@@ -15,55 +15,93 @@ import SearchBox from './components/search-box.component';
 //   return shuffled.slice(min);
 // }
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      monsters: [],
-      searchField: '',
-    };
-  }
+const App = () => {
 
-  componentDidMount() {
+  const [monsters, setMonsters] = useState([])
+  const [searchField, setSearchField] = useState('')
+  const [filteredMonters, setFilteredMonsters] = useState(monsters)
+
+  useEffect(() => {
     fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php?type=normal%20monster')
       .then((response) => response.json())
-      .then((users) => this.setState(
-        () => {
-          // const randomIndex = Math.floor(Math.random() * (users.data.length - 10));
-          return { monsters: users.data }
-          // return { monsters: getRandomValues(users.data, 10) }
-        },
-      ))
-    //   axios.get('https://jsonplaceholder.typicode.com/users')
-    //     .then((users) => this.setState({ monsters: users.data }))
-    //     .then(() => {
-    //       console.log(this.state)
-    //     })
-  }
-
-  onSearchChange = (event) => {
-    const searchValue = event.target.value.toLocaleLowerCase()
-    this.setState(() => {
-      return { searchField: searchValue }
-    })
-  }
+      .then((users) => setMonsters(users.data))
+  }, [])
 
 
-  render() {
-    const { monsters, searchField } = this.state
-    const { onSearchChange } = this;
-    const filtredMonters = this.state.monsters.filter((monster) => {
+  useEffect(() => {
+    const newFilteredMonters = monsters.filter((monster) => {
       return monster.name.toLocaleLowerCase().includes(searchField)
     })
-    return (
+    setFilteredMonsters(newFilteredMonters)
+  }, [monsters, searchField])
 
-      <div className='App' >
-        <img src='https://toppng.com/download/yet8GsuEDa1mqyhdVXdU1pX8MeQa2eFADHNVHBjWOI80CHo1yzWCmCOBz8SBbPzlfMzhWj4dIqCmtl9CPdqwhQKF7YEYmFg4xo1OcJTDRYdbfXJEwhLfP6Gzkrw3eFEWIlt4CYTCtN9QSUGT4G7x7Ly9osgq6XPrIaXLWTPA6Hqilo6ktuX8LejZjDGMa1h2f1vqvOeg/large' alt='logo' className='logo' />
-        <SearchBox onChangeHandler={onSearchChange} placeholder='search monsters' className='search-box' />
-        <CardList monsters={filtredMonters} />
-      </div>
-    );
-  };
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase()
+    setSearchField(searchFieldString)
+  }
+
+
+  return (
+    <div className='App' >
+      <img src='https://toppng.com/download/yet8GsuEDa1mqyhdVXdU1pX8MeQa2eFADHNVHBjWOI80CHo1yzWCmCOBz8SBbPzlfMzhWj4dIqCmtl9CPdqwhQKF7YEYmFg4xo1OcJTDRYdbfXJEwhLfP6Gzkrw3eFEWIlt4CYTCtN9QSUGT4G7x7Ly9osgq6XPrIaXLWTPA6Hqilo6ktuX8LejZjDGMa1h2f1vqvOeg/large' alt='logo' className='logo' />
+      <SearchBox
+        onChangeHandler={onSearchChange}
+        placeholder='search monsters'
+        className='search-box' />
+      <CardList monsters={filteredMonters} />
+    </div>
+  )
 }
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       monsters: [],
+//       searchField: '',
+//     };
+//   }
+
+//   componentDidMount() {
+//     fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php?type=normal%20monster')
+//       .then((response) => response.json())
+//       .then((users) => this.setState(
+//         () => {
+//           return { monsters: users.data }
+//           // return { monsters: getRandomValues(users.data, 10) }
+//         },
+//       ))
+//     //   axios.get('https://jsonplaceholder.typicode.com/users')
+//     //     .then((users) => this.setState({ monsters: users.data }))
+//     //     .then(() => {
+//     //       console.log(this.state)
+//     //     })
+//   }
+
+//   onSearchChange = (event) => {
+//     const searchValue = event.target.value.toLocaleLowerCase()
+//     this.setState(() => {
+//       return { searchField: searchValue }
+//     })
+//   }
+
+
+//   render() {
+//     const { monsters, searchField } = this.state
+//     const { onSearchChange } = this;
+//     const filtredMonters = this.state.monsters.filter((monster) => {
+//       return monster.name.toLocaleLowerCase().includes(searchField)
+//     })
+//     return (
+
+//       <div className='App' >
+//         <img src='https://toppng.com/download/yet8GsuEDa1mqyhdVXdU1pX8MeQa2eFADHNVHBjWOI80CHo1yzWCmCOBz8SBbPzlfMzhWj4dIqCmtl9CPdqwhQKF7YEYmFg4xo1OcJTDRYdbfXJEwhLfP6Gzkrw3eFEWIlt4CYTCtN9QSUGT4G7x7Ly9osgq6XPrIaXLWTPA6Hqilo6ktuX8LejZjDGMa1h2f1vqvOeg/large' alt='logo' className='logo' />
+//         <SearchBox onChangeHandler={onSearchChange} placeholder='search monsters' className='search-box' />
+//         <CardList monsters={filtredMonters} />
+//       </div>
+//     );
+//   };
+// }
 
 export default App;
--- src/components/card-list.component.jsx
+++ src/components/card-list.component.jsx
@@ -1,20 +1,15 @@
-import { React, Component } from "react";
+import { React } from "react";
 import './card-list.styles.css'
 import Card from "./card.component";
 
-class CardList extends Component {
-    render() {
-        const { monsters } = this.props
-        return (
-            <div className='card-list'>
-                {monsters.map((monster) => {
-                    return (
-                        <Card monster={monster} />
-                    )
-                })}
-            </div>
-        )
-    }
-}
+const CardList = ({ monsters }) => (
+    <div className='card-list'>
+        {monsters.map((monster) => {
+            return (
+                <Card monster={monster} />
+            )
+        })}
+    </div>
+)
 
 export default CardList;
